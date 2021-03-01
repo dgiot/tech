28cefdecac1a33af9fd50ba9ad3f8cadc395e067
johnliu <johnliu@iotn2n.com>
2021-03-01 15:56:48 +0800

    增加规则引擎

--- develop/meta.header.js
+++ develop/meta.header.js
@@ -6,6 +6,23 @@ module.exports = [
     "category": "工业数据云平台"
   },
   {
+    "title": "模块管理",
+    "sideTitle": "产品介绍",
+    "path": "product_presentation",
+    "category": "工业数据云平台"
+  },
+  {
+    "title": '规则引擎',
+    "collapsable": true,
+    "children": [
+      ['rule-engine', '规则引擎'],
+      ['rule-create', '创建规则'],
+      ['rule-example', '检查 (调试)'],
+      ['bridge_emqx', '桥接数据到 RPC 服务']
+    ],
+    "category": "工业数据云平台"
+  },
+  {
     "title": 'SDK & Tools',
     "collapsable": true,
     "children": [

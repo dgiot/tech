2c013d42e66492b6aa397e274afbb83159ee410e
johnliu <johnliu@iotn2n.com>
2021-03-01 14:30:39 +0800

    add sdk & tools“

--- develop/meta.header.js
+++ develop/meta.header.js
@@ -4,5 +4,21 @@ module.exports = [
     "sideTitle": "产品介绍",
     "path": "product_presentation",
     "category": "工业数据云平台"
-  }
+  },
+  {
+    "title": 'SDK & Tools',
+    "collapsable": true,
+    "children": [
+      ['Client', 'MQTT Client Library'],
+      ['C_Client', 'MQTT C Client Library'],
+      ['Java_Client', 'MQTT Java Client Library'],
+      ['Erlang_Client', 'MQTT Erlang Client Library'],
+      ['Go_Client', 'MQTT Go Client Library'],
+      ['Python_Client', 'MQTT Python Client Library'],
+      ['JavaScript_Client', 'MQTT JavaScript Client Library'],
+      ['WeChat_Mini_Program', 'MQTT 微信小程序接入'],
+      ['other_resourse', '其他资源']
+    ],
+    "category": "工业数据云平台"
+  },
 ]

c7bd23fcbddefb26c86e4f970ff8b4992fc55219
johnliu <johnliu@iotn2n.com>
2021-03-11 18:12:56 +0800

    增加产品架构

--- zh/product/dgiot/meta.header.js
+++ zh/product/dgiot/meta.header.js
@@ -1,18 +1,13 @@
 module.exports = [
   {
-    "title": '一站式物联网平台',
-    "collapsable": true,
-    "children": [
-      ['./', '什么是数蛙一站式物联网平台']
-    ],
-    "category": "工业数据云平台"
-  },
-  {
     "title": '产品简介',
     "collapsable": true,
     "children": [
-      ['product_presentation/', '什么是物联网平台']
+      ['product_presentation/', '什么是物联网平台'],
+      ['product_functions/', '产品功能'],
+      ['product_architecture/', '产品架构'],
+      ['word_construction/', '名词解释']
     ],
-    "category": "工业数据云平台"
+    "category": "一站式物联网平台"
   }
 ]

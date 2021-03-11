a6aa4a15c5ff3875ab22d5edd8c265fd40283549
johnliu <johnliu@iotn2n.com>
2021-03-11 19:00:16 +0800

    增加产品功能

--- zh/product/dgiot/meta.header.js
+++ zh/product/dgiot/meta.header.js
@@ -4,9 +4,19 @@ module.exports = [
     "collapsable": true,
     "children": [
       ['product_presentation/', '什么是物联网平台'],
-      ['product_functions/', '产品功能'],
-      ['product_architecture/', '产品架构'],
-      ['word_construction/', '名词解释']
+      ['product_presentation/product_functions', '产品功能'],
+      ['product_presentation/product_architecture', '产品架构'],
+      ['product_presentation/word_construction', '名词解释'],
+      ['product_presentation/product_advantage', '产品优势'],
+      ['product_presentation/application_scenarios', '应用场景']
+    ],
+    "category": "一站式物联网平台"
+  },
+  {
+    "title": '设备接入',
+    "collapsable": true,
+    "children": [
+      ['cmp/', '创建产品']
     ],
     "category": "一站式物联网平台"
   }

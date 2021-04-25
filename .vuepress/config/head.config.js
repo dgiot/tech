cf0c293df4dcb752fa6b7701dd13828d25736d88
h7ml <h7ml@qq.com>
2021-04-25 20:59:27 +0800

    docs(other):

--- .vuepress/config/head.config.js
+++ .vuepress/config/head.config.js
@@ -12,6 +12,10 @@ const head =[
     src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js'
   }],
   ['script', {
+    type: 'text/javascript',
+    src: 'https://s9.cnzz.com/z_stat.php?id=1279876880&web_id=1279876880'
+  }],
+  ['script', {
     src: 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.min.js'
   }],
   ['link', {

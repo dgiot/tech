abfe7ff865d315245877b29430bcaab9c5d73dbb
U-JOHNLIU\jonhl <lsxredrain>
2021-05-12 15:49:23 +0800

    fix bug

--- zh/product/dgtest/meta.header.js
+++ zh/product/dgtest/meta.header.js
@@ -3,6 +3,7 @@ module.exports = [
         "title": '数蛙云检测',
         "collapsable": true,
         "children": [
+
             ['word_construction', '基础知识']
         ],
         "category": "一站式物联网云检测平台"
